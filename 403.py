66e1c84aa468b1704b8d10fe123dd53a9f84cd34
unmd1 <rmatoofi@gmail.com>
2024-08-19 10:32:20 +0330

    bug fix

--- 403.py
+++ 403.py
@@ -4,4 +4,4 @@ rc = recon.recon(prjID=prjID)
 query = "SELECT `address` FROM `subs` WHERE `prj` = '" + rc.prjname +r"' and `result` LIKE '%forbidden%'"
 urls = rc.mysql_show(query)
 for url in urls:
-    rc.forbidden_solve(url)
+    rc.forbidden_solve(url["address"])
