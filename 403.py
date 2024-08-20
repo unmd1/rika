ea7ece8a4aa8df1fb7c566a3264fd1228f185bc5
unmd1 <rmatoofi@gmail.com>
2024-08-20 10:31:03 +0330

    403 dont duplicate

--- 403.py
+++ 403.py
@@ -3,7 +3,11 @@ import json
 prjID = "1"
 rc = recon.recon(prjID=prjID)
 query = "SELECT `result` FROM `subs` WHERE `prj` = '" + rc.prjname +r"' and `result` LIKE '%forbidden%'"
-urls = rc.mysql_show(query)
+urls=[]
+urls_d = rc.mysql_show(query)
+for url in urls_d:
+    url_d_l = json.loads(url["result"])["url"]
+    if not url_d_l in urls:
+        urls.append(url_d_l)
 for url in urls:
-    url_org = json.loads(url["result"])["url"]
-    rc.forbidden_solve(url_org)
+    rc.forbidden_solve(url)
